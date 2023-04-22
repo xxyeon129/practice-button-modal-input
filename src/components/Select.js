8aa1043562591a2318644f3678994002807668e2
xxyeon129 <xxyeon129@gmail.com>
2023-04-22 14:55:52 +0900

    [feat] Custom Select 구현

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,44 +1,75 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function Select() {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selectOption, setSelectOption] = useState(null);
+
+    const options = ["React", "JAVA", "Spring", "React Native"];
+
+    const optionClickHandler = (option) => {
+        setSelectOption(option);
+        setIsOpen(false);
+    };
+
     return (
         <SelectContainer>
             <h1>Select</h1>
-            <SelectWrapperStyle>
-                <select>
-                    <option value="" selected>
-                        리액트
-                    </option>
-                    <option>리액트</option>
-                    <option>자바</option>
-                    <option>스프링</option>
-                    <option>리액트 네이티브</option>
-                </select>
-                <select>
-                    <option value="" selected>
-                        리액트
-                    </option>
-                    <option>리액트</option>
-                    <option>자바</option>
-                    <option>스프링</option>
-                    <option>리액트 네이티브</option>
-                </select>
-            </SelectWrapperStyle>
+            <SelectContentBox>
+                <SelectWrapperStyle>
+                    <DropdownMain onClick={() => setIsOpen(!isOpen)}>
+                        {selectOption || "React"}
+                    </DropdownMain>
+                    {isOpen && (
+                        <DropdownListContainer>
+                            <DropdownList>
+                                {options.map((option) => (
+                                    <ListItem
+                                        onClick={() =>
+                                            optionClickHandler(option)
+                                        }
+                                    >
+                                        {option}
+                                    </ListItem>
+                                ))}
+                            </DropdownList>
+                        </DropdownListContainer>
+                    )}
+                </SelectWrapperStyle>
+                <SelectWrapperStyle>
+                    {/* <select>
+                        <option value="" selected>
+                            리액트
+                        </option>
+                        <option>리액트</option>
+                        <option>자바</option>
+                        <option>스프링</option>
+                        <option>리액트 네이티브</option>
+                    </select> */}
+                </SelectWrapperStyle>
+            </SelectContentBox>
         </SelectContainer>
     );
 }
 
 const SelectContainer = styled.div`
     margin-top: 2.5rem;
-    height: 10rem;
+    height: 13rem;
     border: 3px solid rgb(221, 221, 221);
     border-radius: 1rem;
     padding: 0 2rem;
-    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    /* overflow: hidden; */
+`;
+
+const SelectContentBox = styled.div`
+    display: flex;
 `;
 
 const SelectWrapperStyle = styled.div`
     display: flex;
+    flex-direction: column;
     gap: 1rem;
 
     select {
@@ -51,5 +82,45 @@ const SelectWrapperStyle = styled.div`
         padding: 0.8rem 1rem;
         border: 1px solid rgb(221, 221, 221);
         border-radius: 0.5rem;
+    }
+`;
+
+const DropdownMain = styled.div`
+    width: 20rem;
+    padding: 0.8rem 1rem;
+    border: 1px solid rgb(221, 221, 221);
+    border-radius: 0.5rem;
+    cursor: pointer;
+`;
+
+const DropdownListContainer = styled.div`
+    border: 1px solid rgb(221, 221, 221);
+    border-radius: 0.5rem;
+    padding: 0;
+    background-color: white;
+    cursor: pointer;
+`;
+
+const DropdownList = styled.ul`
+    list-style: none;
+    padding: 0;
+    margin: 0;
+`;
+
+const ListItem = styled.li`
+    padding: 1rem 0.9rem;
+
+    :first-child {
+        border-top-right-radius: 0.5rem;
+        border-top-left-radius: 0.5rem;
+    }
+
+    :last-child {
+        border-bottom-left-radius: 0.5rem;
+        border-bottom-right-radius: 0.5rem;
+    }
+
+    :hover {
+        background-color: rgb(221, 221, 221);
     }
 `;
