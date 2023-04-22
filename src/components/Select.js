032f863299b62a86ec283d9b9bdb5bed292f1ee3
xxyeon129 <xxyeon129@gmail.com>
2023-04-22 12:25:33 +0900

    [feat] select 기본 설정 구현

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,1 +1,55 @@
-export default function Select() {}
+import styled from "styled-components";
+
+export default function Select() {
+    return (
+        <SelectContainer>
+            <h1>Select</h1>
+            <SelectWrapperStyle>
+                <select>
+                    <option value="" selected>
+                        리액트
+                    </option>
+                    <option>리액트</option>
+                    <option>자바</option>
+                    <option>스프링</option>
+                    <option>리액트 네이티브</option>
+                </select>
+                <select>
+                    <option value="" selected>
+                        리액트
+                    </option>
+                    <option>리액트</option>
+                    <option>자바</option>
+                    <option>스프링</option>
+                    <option>리액트 네이티브</option>
+                </select>
+            </SelectWrapperStyle>
+        </SelectContainer>
+    );
+}
+
+const SelectContainer = styled.div`
+    margin-top: 2.5rem;
+    height: 10rem;
+    border: 3px solid rgb(221, 221, 221);
+    border-radius: 1rem;
+    padding: 0 2rem;
+    overflow: hidden;
+`;
+
+const SelectWrapperStyle = styled.div`
+    display: flex;
+    gap: 1rem;
+
+    select {
+        /* css 설정 초기화 */
+        -moz-appearance: none;
+        -webkit-appearance: none;
+        appearance: none;
+
+        width: 20rem;
+        padding: 0.8rem 1rem;
+        border: 1px solid rgb(221, 221, 221);
+        border-radius: 0.5rem;
+    }
+`;
